f6c95d6e190d9f79387c11e41c78168472156546
Anna Andrienko <milesha10@inbox.ru>
2022-07-17 21:07:28 +0700

    task10 has been changed

--- task10/Program.cs
+++ task10/Program.cs
@@ -5,8 +5,7 @@
 //918 -> 1
 Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int number1 = number / 100;
+
 int number2 = number / 10 % 10;
-int number3 = number % 10;
-int result = number1*10 + number3;
-Console.WriteLine($"Решение задачи={result}");
+
+Console.WriteLine($"Решение задачи={number2}");
